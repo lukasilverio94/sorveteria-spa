092c5e7b3dc45cc8ef4af52f4d342e17cd3213d4
lukasilverio94 <lukasilverio94@gmail.com>
2023-08-18 09:58:10 +0200

    header mobile responsive edit

--- src/rotas/index.js
+++ src/rotas/index.js
@@ -1,18 +1,17 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Home from '../pages/Home';
-import Sabores from '../pages/Sabores';
-import Sobre from '../pages/Sobre';
+import Home from "../pages/Home";
+import Sabores from "../pages/Sabores";
+import Sobre from "../pages/Sobre";
 
 export default function Rotas() {
-
-  return(
+  return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home />}/>
-        <Route path='/sabores' element={<Sabores />}/>
-        <Route path='/sobre' element={<Sobre />}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/sabores" element={<Sabores />} />
+        <Route path="/sobre" element={<Sobre />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
