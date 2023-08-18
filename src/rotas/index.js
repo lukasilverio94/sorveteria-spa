2510b20609687b23dbd6a27e5253a37008f1c5f8
lukasilverio94 <lukasilverio94@gmail.com>
2023-08-18 11:55:28 +0200

    Component ScrollToTop added

--- src/rotas/index.js
+++ src/rotas/index.js
@@ -1,12 +1,25 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React, {useEffect} from 'react';
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 
 import Home from "../pages/Home";
 import Sabores from "../pages/Sabores";
 import Sobre from "../pages/Sobre";
 
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  // You need to return null or some JSX element from a functional component
+  return null;
+};
+
 export default function Rotas() {
   return (
     <BrowserRouter>
+    <ScrollToTop/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sabores" element={<Sabores />} />
